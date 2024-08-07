42b9911f45cddb85233303384b7e2684a78fd781
Mndraca <marizanovic3@gmail.com>
2024-08-07 09:37:44 +0200

    api

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,23 +1,25 @@
 import React, { FC } from "react";
 
 interface ButtonProps {
-    name: string;
-    handleClick: () => void;
+  name: string;
+  handleClick: (event: React.FormEvent) => void;
 }
 
+const Button = ({ name, handleClick }: ButtonProps) => {
+  const upperCapB = (name: string) => {
+    return name.slice(0, 1).toUpperCase() + name.slice(1);
+  };
 
-const Button = ({name, handleClick}: ButtonProps) =>{
-
-    const upperCapB = (name: string) => {
-        return name.slice(0, 1).toUpperCase() + name.slice(1);
-        }
-
-return(
+  return (
     <>
-    <button className="border border-transparent bg-gradient-to-br from-rose-300 via-rose-400 to-rose-300 text-black w-full sm:w-1/2 rounded-full shadow-2xl shadow-rose-700 transition h-8 hover:opacity-75 mt-8 font-bold" onClick={handleClick} >{upperCapB(name)}</button>
+      <button
+        className="border border-transparent bg-gradient-to-br from-rose-300 via-rose-400 to-rose-300 text-black w-full sm:w-1/2 rounded-full shadow-2xl shadow-rose-700 transition h-8 hover:opacity-75 mt-8 font-bold"
+        onClick={handleClick}
+      >
+        {upperCapB(name)}
+      </button>
     </>
-)
-
-}
+  );
+};
 
 export default Button;
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,74 +1,110 @@
 import React, { ChangeEvent, FC, useState } from "react";
-import './layout.css'
+import "./layout.css";
 import Input from "./input";
 import Button from "./button";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
-
-
-
 const Contact: FC = () => {
+  const [formDataName, setFormDataName] = useState<string>("");
+  const [formDataEmail, setFormDataEmail] = useState<string>("");
+  const [formDataMessage, setFormDataMessage] = useState<string>("");
 
-  const [formDataName, setFormDataName] = useState<string>('');
-  const [formDataEmail, setFormDataEmail] = useState<string>('');
-  const [formDataMessage, setFormDataMessage] = useState<string>('');
+  const handleChangeName = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormDataName(event.target.value);
+  };
 
+  const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormDataEmail(event.target.value);
+  };
 
- const handleChangeName = (event: ChangeEvent<HTMLInputElement>) =>{
-  setFormDataName(event.target.value);
- }
-
- const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>) =>{
-  setFormDataEmail(event.target.value);
-  }
-
-  const handleChangeMessage = (event: ChangeEvent<HTMLInputElement>) =>{
+  const handleChangeMessage = (event: ChangeEvent<HTMLInputElement>) => {
     setFormDataMessage(event.target.value);
-    }
+  };
 
-    const turnRed = () => {
-
+  const validateFields = () => {
     const emptyFields: string[] = [];
     if (!formDataName) emptyFields.push("Name");
     if (!formDataEmail) emptyFields.push("Email");
     if (!formDataMessage) emptyFields.push("Message");
-    if (emptyFields.length > 0){
-      alert(`Please fill up empty fields ${emptyFields.join(" ,")}`)
-    } else {
-      console.log("All good!")
+    return emptyFields;
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    const emptyFields = validateFields();
+    if (emptyFields.length > 0) {
+      alert(`Please fill up empty fields: ${emptyFields.join(", ")}`);
+      return;
     }
 
+    const data = {
+      full_name: formDataName,
+      email_address: formDataEmail,
+      message: formDataMessage,
+    };
+
+    try {
+      const responce = await fetch(
+        "https://api.sheetmonkey.io/form/e2UUyfFHWbV41PXCduWivU",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (responce.ok) {
+        console.log("You did great!");
+      }
+    } catch (error) {
+      console.error("Your submission failed:", error);
     }
+  };
 
-
-
-
-
-
-  return(
-<>
-  <section className="grid md:grid-cols-2 md:mt-12 py-20 gap-4 bg-pink-200 border rounded p-8 mt-10">
-    <div>
-        <h5 className="text-xl font-bold text-black my-2">Let's Connect.</h5>
-        <div className="max-w-20 flex flex-row justify-between md:flex-col align-center">
-         <a href="https://github.com/Mndraca" target="_blank" rel="noopener noreferrer"><i><FaGithub className="md:mb-3 md:mt-3 h-5 w-5"/></i></a>
-          <a href="https://www.linkedin.com/in/milena-komnenic-75835051/" target="_blank" rel="noopener noreferrer"><i><FaLinkedin className="md:mb-3 md:mt-3 h-5 w-5"/></i></a>
-          <a href="#"><i><FaXTwitter className="md:mb-3 md:mt-3 h-5 w-5"/></i></a>
+  return (
+    <>
+      <section className="grid md:grid-cols-2 md:mt-12 py-20 gap-4 bg-pink-200 border rounded p-8 mt-10">
+        <div>
+          <h5 className="text-xl font-bold text-black my-2">Let's Connect.</h5>
+          <div className="max-w-20 flex flex-row justify-between md:flex-col align-center">
+            <a
+              href="https://github.com/Mndraca"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i>
+                <FaGithub className="md:mb-3 md:mt-3 h-5 w-5" />
+              </i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/milena-komnenic-75835051/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i>
+                <FaLinkedin className="md:mb-3 md:mt-3 h-5 w-5" />
+              </i>
+            </a>
+            <a href="#">
+              <i>
+                <FaXTwitter className="md:mb-3 md:mt-3 h-5 w-5" />
+              </i>
+            </a>
+          </div>
         </div>
-    </div>
-    <div id="contact">
-      <Input name= "name" handleChange = {handleChangeName}  />
-      <Input name= "email" handleChange = {handleChangeEmail} />
-      <Input name= "message" handleChange = {handleChangeMessage}/>
-      <Button name ="Submit" handleClick={turnRed}/>
-    </div>
-  </section>
-</>
-
-  )
-
+        <form onSubmit={handleSubmit}>
+          <div id="contact">
+            <Input name="name" handleChange={handleChangeName} />
+            <Input name="email" handleChange={handleChangeEmail} />
+            <Input name="message" handleChange={handleChangeMessage} />
+            <Button name="Submit" handleClick={handleSubmit} />
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
