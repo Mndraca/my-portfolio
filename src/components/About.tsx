e2eb6da6bb85d1e6329b72f2ff547be3941c61f9
Mndraca <marizanovic3@gmail.com>
2024-08-07 12:34:10 +0200

    added bg video

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,42 @@
-import React, { FC } from 'react';
-import './layout.css';
-import Button from './button';
+import React, { FC } from "react";
+import "./layout.css";
+import Button from "./button";
+import VideoBG from "../assets/VideoBG.mp4";
 
 const About: FC = () => {
   return (
-    <section className="bg-pink-200 border rounded-md p-10 md:p-20"id="about">
-      <div>
-        <h2 className='text-6xl mb-4'>Hello!</h2>
-        <p> 
-          My name is Milena Komnenic. 
-          My interests are in Front End Engineering, and I love to create beautiful and performant products with delightful user experiences.
-          I like to code things from scratch, and enjoy bringing ideas to life in the browser.
+    <section
+      className="relative bg-pink-200 border rounded-md p-10 md:p-20"
+      id="about"
+    >
+      <div className="absolute inset-0 overflow-hidden">
+        <video
+          className="w-full h-full object-cover"
+          src={VideoBG}
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+      <div className="relative z-10">
+        <h2 className="text-6xl mb-4">Hello!</h2>
+        <p>
+          My name is Milena Komnenic. My interests are in Front End Engineering,
+          and I love to create beautiful and performant products with delightful
+          user experiences. I like to code things from scratch, and enjoy
+          bringing ideas to life in the browser.
         </p>
-        <a href="#contact"><button className="border border-transparent bg-gradient-to-br from-rose-300 via-rose-400 to-rose-300 text-black w-full sm:w-1/2 rounded-full shadow-2xl shadow-rose-700 transition h-8 hover:opacity-75 mt-4 font-bold" type='button'>Message Me!</button></a>
+        <a href="#contact">
+          <button
+            className="border border-transparent bg-gradient-to-br from-rose-300 via-rose-400 to-rose-300 text-black w-full sm:w-1/2 rounded-full shadow-2xl shadow-rose-700 transition h-8 hover:opacity-75 mt-4 font-bold"
+            type="button"
+          >
+            Message Me!
+          </button>
+        </a>
       </div>
     </section>
   );
-}
+};
 
 export default About;
