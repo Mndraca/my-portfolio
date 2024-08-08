f3e8edafef75a0827ffd9fabae71b08ac3b3c2cc
Mndraca <marizanovic3@gmail.com>
2024-08-08 16:25:07 +0200

    styled-resume-for-sm

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+
 const Resume: React.FC = () => {
   return (
-    <div>
+    <div className="mt-4 sm:mt-0">
       <a
         href="/resume.pdf"
         download="Milena_Komnenic_Resume.pdf"
-        className="bg-red-300 text-white py-1 px-2 rounded hover:bg-pink-400 mt-4"
+        className="bg-red-300 text-white py-1 px-2 rounded hover:bg-pink-400 block sm:inline-block"
       >
         Download Resume
       </a>
