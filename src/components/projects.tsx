dbfdf82c5252f1524d329adf7fd252570b52da45
Mndraca <marizanovic3@gmail.com>
2024-08-11 10:39:39 +0200

    look_here-style

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,38 +3,28 @@ import "./layout.css";
 
 const Projects: FC = () => {
   return (
-    <section id="projects" className="my-10  ">
+    <section id="projects" className="my-10">
       <h2 className="text-5xl mb-4 text-transparent bg-clip-text bg-gradient-to-r from-rose-400 via-rose-200 to-rose-500">
         Projects
       </h2>
       <div className="project-list">
         <div className="project">
           <h3 className="font-bold text-xl mb-4">Book Review App</h3>
-          <p className="mb-4">
+          <p className="mb-16">
             Book Review App is a form of literary criticism in which a book is
-            merely described or analyzed based on content, style, and merit.{" "}
+            merely described or analyzed based on content, style, and merit.
           </p>
           <a
             href={"https://mndraca.github.io/BookReview-/"}
             target="_blank"
-            className="text-pink-400"
-            style={{
-              textDecoration: "none",
-              padding: "6px 8px",
-              borderRadius: "4px",
-              backgroundColor: "#FF69B4",
-              color: "white",
-              fontWeight: "bold",
-              display: "inline-block",
-              marginTop: "8px",
-            }}
+            className="inline-block text-white bg-black hover:bg-gray-800 font-bold py-2 px-4 rounded"
           >
             Look here
           </a>
         </div>
         <div className="project">
           <h3 className="font-bold text-xl mb-4">Fitness Tracker</h3>
-          <p className="mb-4">
+          <p className="mb-10">
             A fitness tracking app is a mobile application designed to help
             users monitor and manage their physical activity, health metrics,
             and wellness goals.
@@ -42,39 +32,36 @@ const Projects: FC = () => {
           <a
             href={"https://sdzika.github.io/workout-logging-form/"}
             target="_blank"
-            className="text-pink-400"
-            style={{ textDecoration: "none" }}
+            className="inline-block text-white bg-black hover:bg-gray-800 font-bold py-2 px-4 rounded"
           >
             Look here
           </a>
         </div>
         <div className="project">
           <h3 className="font-bold text-xl mb-4">Sita Dini Magazine</h3>
-          <p className="mb-4">
+          <p className="mb-16">
             Lifestyle journalism covers travel, fashion, fitness, leisure, food,
-            and arts, among other topics.
+            and arts, among other topics. Find out what is new out there.
           </p>
           <a
             href={"https://mndraca.github.io/sitadini/"}
             target="_blank"
-            className="text-pink-400"
-            style={{ textDecoration: "none" }}
+            className="inline-block text-white bg-black hover:bg-gray-800 font-bold py-2 px-4 rounded"
           >
             Look here
           </a>
         </div>
         <div className="project">
           <h3 className="font-bold text-xl mb-4">Multi-Function Tool</h3>
-          <p className="mb-4">
+          <p className="mb-8">
             The App can calculate and display the factorial of a number, reverse
-            a given word, convert text to uppercase,looping constructs through a
-            sequence.
+            a given word, convert text to uppercase, looping constructs through
+            a sequence.
           </p>
           <a
             href={""}
             target="_blank"
-            className="text-pink-400"
-            style={{ textDecoration: "none" }}
+            className="inline-block text-white bg-black hover:bg-gray-800 font-bold py-2 px-4 rounded"
           >
             Look here
           </a>
