e7263b86d027c6e5843b28b2e1b49023af65843f
Mndraca <marizanovic3@gmail.com>
2024-08-11 15:05:23 +0200

    button-mobile

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -13,6 +13,7 @@ const Button = ({ name, handleClick }: ButtonProps) => {
   return (
     <>
       <button
+        id="button"
         className="border border-transparent bg-gradient-to-br from-rose-300 via-rose-400 to-rose-300 text-black w-full  ml-16 sm:w-1/2 rounded-full shadow-2xl shadow-rose-700 transition h-8 hover:opacity-75 mt-8 font-bold"
         onClick={handleClick}
       >
